4f4fb3c1e19b769cb0a94f1b1ed655c5e0b5ded5
Lova Long （龙华） <Lova_H_Long@manulife.com>
2021-11-08 11:26:29 +0800

    init package port

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# go-port
+## goport
+
+
+#### install
+
+Install the package with:
+```
+go get https://github.com/laof/goport
+```
+
+Import it with:
+```
+go get https://github.com/laof/goport
+```
+
+#### Example
+```go
+ip := goport.GetIp()
+port := goport.InputPort("8918")
+```
---
+++ port.go
@@ -0,0 +1,45 @@
+package port
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func InputPort(str string) string {
+
+	fmt.Print("Port default is " + str + ", Input:")
+
+	in := bufio.NewScanner(os.Stdin)
+	if in.Scan() {
+		port := in.Text()
+		if len(port) >= 4 {
+			str = port
+		}
+	}
+
+	port := ":" + str
+
+	fmt.Println("http://localhost" + port)
+	fmt.Println("http://" + GetIP() + port)
+
+	return str
+}
+
+func GetIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	var ip string
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				ip = ipnet.IP.String()
+			}
+		}
+	}
+	return ip
+}
